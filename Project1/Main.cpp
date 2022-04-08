d29909777600c80a900b3ef6708a736cf5144f4c
Tang <brightrobber@gmail.com>
2022-04-08 22:55:19 +0200

    ex 3

--- Project1/Main.cpp
+++ Project1/Main.cpp
@@ -225,7 +225,8 @@ int main()
             glm::mat4 model = glm::mat4(1.0f);
             model = glm::translate(model, cubePositions[i]);
             float angle = 20.0f * i;
-            model = glm::rotate(model, (float)glfwGetTime() * glm::radians(angle), glm::vec3(1.0f, 0.3f, 0.5f));
+            float rotation_para = i % 3 == 0 ? (float)glfwGetTime() : 1.0f;
+            model = glm::rotate(model, rotation_para * glm::radians(angle), glm::vec3(1.0f, 0.3f, 0.5f));
             ourShader.setMat4("model", model);
 
             glDrawArrays(GL_TRIANGLES, 0, 36);
