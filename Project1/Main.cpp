6b60ca6b429855a70ffa295eb33ea0b3e8df51fe
Tang <brightrobber@gmail.com>
2022-04-11 10:03:17 +0200

    ex.1 changing cube light

--- Project1/Main.cpp
+++ Project1/Main.cpp
@@ -280,6 +280,7 @@ int main()
         glDrawArrays(GL_TRIANGLES, 0, 36);
 
         lightCubeShader.use();
+        lightCubeShader.setVec3("lightColor", lightColor);
         lightCubeShader.setMat4("projection", projection);
         lightCubeShader.setMat4("view", view);
         model = glm::mat4(1.0f);
